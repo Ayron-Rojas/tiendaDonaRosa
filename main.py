16241645de764667ceb859785ea2e22008e14b32
Jairo Pantoja <jairo.pantoja001@gmail.com>
2024-02-27 09:28:07 -0300

    Agregado multiples comandos de saludo

--- main.py
+++ main.py
@@ -7,10 +7,14 @@ bot = telebot.TeleBot(TOKEN)
 @bot.message_handler(commands=["start"])
 def saludar(mensaje):
     bot.reply_to(mensaje, "Hola, ¿en qué te puedo ayudar?")
+    
+@bot.message_handlers(commands=["Hola", "hola", "hi", "Hi"])
+def saludar(mensaje):
+    bot.reply_to(mensaje, "Hola, ¿en qué te puedo ayudar?")
 
 @bot.message_handler(func = lambda m: True)
 def escucha_todo(mensaje):
-    bot.reply_to(mensaje, "procesando")
+    bot.reply_to(mensaje, "Actualmente solo estoy funcionando con los comandos '/start y /hola'")
 
 if __name__ == "__main__":
     bot.polling(none_stop=True)
